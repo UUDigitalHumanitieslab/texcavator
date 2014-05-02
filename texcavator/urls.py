cd196935345c1393131de8c18d56fc5a8d5193c7
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-05-02 16:14:11 +0200

    Cleaned main url configuration
    Removed superfluous url configurations and comments from
    texcavator/urls.py.

--- texcavator/urls.py
+++ texcavator/urls.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
---------------------------------------------------------------------------------
-Copyright:	Fons Laan, ILPS-ISLA, University of Amsterdam
-Project		BiLand
-Name:		urls.py
-Version:	0.22
-Goal:		main url definitions
-
-DO-%%-%%%-2011: Created
-FL-28-Mar-2012: Added django-socialregistration (OAuth for Clarin)
-FL-04-Jul-2013: -> BILAND app
-FL-26-Aug-2013: Changed
-"""
-
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
@@ -26,42 +12,32 @@ from django.http import HttpResponse
 from django.views.generic import RedirectView, TemplateView
 urlpatterns = patterns( '',
 	url( r'^favicon\.ico$', RedirectView.as_view( url = '/static/image/icon/favicon.ico' ) ),
-	url( r'^js/tests/$',    RedirectView.as_view( url = '/js/tests/index.html' ) ),
-	url( r'^robots\.txt$',  TemplateView.as_view( template_name = 'robots.txt', content_type = 'text/plain' ) ),
+	url( r'^robots\.txt$', TemplateView.as_view( template_name = 'robots.txt', content_type = 'text/plain' ) ),
 )
 
-
 urlpatterns += patterns( '', 
-	# Examples:
-	# url( r'^$', 'BILAND.views.home', name='home' ),
-	# url( r'^BILAND/', include('BILAND.foo.urls') ),
-
 	# Uncomment the admin/doc line below to enable admin documentation:
 	url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
 
 	# Uncomment the next line to enable the admin:
 	url( r'^admin/', include( admin.site.urls) ),
 
-	# OAuth for Clarin
-#	url( r'^social/', include( 'socialregistration.urls', namespace = 'socialregistration' ) ),
-
 	url( r'^js/(?P<path>.*)$', 'django.views.static.serve', { 
 		'document_root':'js/', 
 		'show_indexes': True
 	}),
+	
+	url( r'^$', 'texcavator.views.texcavator', name="index" ),
 
-#	url( r'^services/', 'services.views.proxy' ),
-	url( r'^services/',	include( 'services.urls' ) ),
+	url( r'^login/ajax/$', 'texcavator.views.loginajax' ),
+	
+	url( r'^services/', include( 'services.urls' ) ),
+	url( r'^lexicon/', include( 'lexicon.urls' ) ),
+	url( r'^list/', include( 'list.urls' ) ),
 
-	url( r'^lexicon/',	include( 'lexicon.urls' ) ),
-	url( r'^list/',		include( 'list.urls' ) ),
-
-#	url( r'^accounts/', include( 'registration.backends.default.urls' ) ),
-	url( r'^login/$',		'django.contrib.auth.views.login' ),
-	url( r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name' : 'registration/password_reset.html',  'post_reset_redirect': '/logout/' } ),
-	url( r'^login/ajax/$',	'texcavator.views.loginajax' ),
-	url( r'^texcavator/',	'texcavator.views.texcavator', name="texcavator" ),
-	url( r'^index/',		'texcavator.views.index',      name="index" ),
-	url( r'^$',				'texcavator.views.index',      name="index" ),
-)
-
+	# Not used?
+	url( r'^login/$', 'django.contrib.auth.views.login' ),
+	url( r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+		'django.contrib.auth.views.password_reset_confirm', 
+		{'template_name' : 'registration/password_reset.html',  'post_reset_redirect': '/logout/' } ),
+	)
