ab54e1610b1e3adb9316a444bb8fe8f193d7aa8b
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-05-21 11:51:47 +0200

    Add utility function for returning json error messages
    In Texcavator error messages are communicated to the user interface by
    sending json objects containing the error message. To avoid repeating
    the creation of these error messages, a utility function that returns
    custom error messages was added.
    
    It seems that in the interface, error messages are not always accessed
    in the same way (e.g. field error instead of msg). This still has to be
    updated.

--- texcavator/utils.py
+++ texcavator/utils.py
@@ -1,3 +1,15 @@
 """Utility functions for the Texcavator app"""
 
 
+import json
+from django.http import HttpResponse
+
+
+def json_error_message(message):
+    """Return HttpResponse object specifying an error"""
+    response = json.dumps({
+        'status': 'error',
+        'msg': message
+    })
+
+    return HttpResponse(response, 'application/json; charset=UTF-8')
