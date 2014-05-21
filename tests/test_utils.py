cc0f33a51f3b13cec191a7a97d20af95082e38db
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-05-21 12:48:16 +0200

    Add test for utility function json_error_message()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,15 @@
 """Tests for the Texcavator utility functions"""
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "texcavator.settings")
+
+from nose.tools import assert_equals
+from testfixtures import compare
+
+import texcavator.utils as utils
+
+
+def test_json_error_message():
+    response = utils.json_error_message('test')
+
+    compare(response.content, '{"status": "error", "msg": "test"}')
+    assert_equals(response.status_code, 200)
