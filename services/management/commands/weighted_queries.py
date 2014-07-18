66b5cdbe67019a8b838c75ad33a3ef6a7d6bbb6d
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-07-18 13:16:40 +0200

    Add measurement of wall clock time to weighted queries
    ES performance on weighted queries was reported in the time spend by
    ES. Added measurement of wall clock time.

--- services/management/commands/weighted_queries.py
+++ services/management/commands/weighted_queries.py
@@ -3,6 +3,7 @@
 """Perform searches with random weighted queries in ElasticSearch.
 """
 from random import randint
+import time
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -27,6 +28,7 @@ class Command(BaseCommand):
             n_repetitions = int(args[1])
 
         response_times = []
+        es_wall_clock = []
 
         for repetition in range(n_repetitions):
             # generate random weigthed query
@@ -36,16 +38,22 @@ class Command(BaseCommand):
                           for t in query_terms]
             q = ' OR '.join(query_list)
 
+            t1 = time.time()
             valid_q, result = do_search(settings.ES_INDEX, settings.ES_DOCTYPE,
                                         q, 0, 20, daterange2dates(''), [], [])
+            t2 = time.time()
 
             if not valid_q:
                 print 'Invalid query: {}'.format(q)
             else:
+                es_wall_clock.append((t2-t1)*1000)
                 response_times.append(int(result.get('took')))
                 self.stdout.write(str(result.get('took')))
                 self.stdout.flush()
 
         avg = float(sum(response_times)/len(response_times))
+        avg_wall_clock = float(sum(es_wall_clock)/len(es_wall_clock))
         print 'Average response time for queries of size {}: {} miliseconds'. \
               format(query_size, avg)
+        print 'Average wall clock time for queries of size {}: {} ' \
+              'miliseconds'.format(query_size, avg_wall_clock)
