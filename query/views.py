5607fec2c842870b76a43b9dab9dcb89f6978da2
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-07-31 15:36:52 +0200

    Add view to retrieve query details
    Added a view that retrieves a query based on a query id. Currently,
    only the query and query id are returned. The view needs to be updated
    to return all relevant details (including metadata). Also, a check
    needs to be added to verify whether the current user is allowed to
    retrieve the query.

--- query/views.py
+++ query/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404
+
+from django.conf import settings
+
+from query.models import Query
+from texcavator.utils import json_response_message
+
+
+def query(request, query_id):
+    # TODO: check whether query belongs to the user
+
+    query = get_object_or_404(Query, pk=query_id)
+
+    params = {
+        'query_id': query.id,
+        'query': query.query
+    }
+
+    return json_response_message('OK', '', params)
