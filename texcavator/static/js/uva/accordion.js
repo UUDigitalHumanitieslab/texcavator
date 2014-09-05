51fe3d6b850a63f8f3fd4c605a155839cba84560
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-09-05 14:23:01 +0200

    Fix update (modify) query
    Fixed update (modify) query to read the query, title and comment from
    their respective fields and send them to the back end.

--- texcavator/static/js/uva/accordion.js
+++ texcavator/static/js/uva/accordion.js
@@ -215,7 +215,11 @@ function createQueryLine( item )
 		iconClass: "dijitIconSave",
 		pk: item.pk,
 		onClick: function() {
-            saveQuery(item.fields.comment, item.fields.query, "query/"+item.pk+"/update");
+            var title = dojo.byId("lexiconItemTitle").value;
+            var comment = dojo.byId("queryComment").value;
+            var query = dojo.byId("query").value;
+
+            saveQuery(title, comment, query, "query/"+item.pk+"/update");
 
             createQueryList();
         }
