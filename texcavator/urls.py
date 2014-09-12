710f96573492f1fbd01db8bb646740156d70b53d
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-09-12 10:12:59 +0200

    Add url mapping and view for new login functionality
    An url mapping and view were added for the new login functionality. The
    old login functionality did not call the Django login function, meaning
    that the user was never really logged in. Logging in the user means
    that the standard Django methods for user authentication/verification
    can be used. This will lead to cleaner and more understandable code.
    What still needs to be done: the user interface must call the new login
    functionality. Also, when index.html is loaded, the login dialog is
    always displayed, regardless of whether a user is already logged in.
    This should be fixed.

--- texcavator/urls.py
+++ texcavator/urls.py
@@ -30,6 +30,7 @@ urlpatterns += patterns( '',
 	url( r'^$', 'texcavator.views.index', name="index" ),
 
 	url( r'^login/ajax/$', 'texcavator.views.loginajax' ),
+	url( r'^login$', 'texcavator.views.user_login' ),
 	
 	url( r'^services/', include( 'services.urls' ) ),
 	url( r'^lexicon/', include( 'lexicon.urls' ) ),
--- texcavator/views.py
+++ texcavator/views.py
@@ -20,12 +20,13 @@ from sys import stderr
 import json
 
 from django.conf import settings
-from django.contrib import auth
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 
+from texcavator.utils import json_response_message
 from services.es import daterange2dates
 
 from texcavator.timestamp import TIMESTAMP
@@ -53,6 +54,39 @@ def index( request ):
 	}
 
 	return render_to_response('index.html', data, RequestContext(request))
+
+
+@csrf_exempt
+def user_login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    user = authenticate(username=username, password=password)
+    
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+	
+            # TODO: are these datelimts really necessary?
+            date_limits = daterange2dates('')
+	    dates = [date_limits['lower'], date_limits['upper']]
+	    daterange = [int(d.replace('-', '')) for d in dates]
+	    
+            params = {
+                "user_id"   : user.id,
+                "user_name" : user.username,
+                "daterange" : daterange,
+                # TODO: what is timestamp used for? Is it really necessary
+                "timestamp" : TIMESTAMP
+            }
+
+            return json_response_message('SUCCESS', '', params)
+        else:
+            return json_response_message('ERROR', 'Account disabled.\n' \
+                                         'Please contact the system ' \
+                                         'administrator.')
+
+    return json_response_message('ERROR', 'Oops, that is not correct!' )
 
 
 @csrf_exempt
