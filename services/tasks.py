5669424eb40b1c543a72163960cbe9ac007d72ea
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-09-18 17:31:27 +0200

    Add celery task for generating termvector wordclouds
    Added a celery task that generates a termvector wordcloud. The next step
    is to call this task in the tv_cloud view, so the wordcloud is generated
    asynchonously.

---
+++ services/tasks.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+import json
+from celery import shared_task
+
+from django.conf import settings
+from texcavator.utils import json_response_message
+from services.es import get_document_ids, termvector_word_cloud
+
+
+@shared_task
+def generate_tv_cloud(query):
+    dates = {'lower': query['date_lower'], 'upper': query['date_upper']}
+
+    # get ids
+    doc_ids = get_document_ids(settings.ES_INDEX,
+                               settings.ES_DOCTYPE,
+                               query['query'],
+                               dates,
+                               query['exclude_distributions'],
+                               query['exclude_article_types'])
+
+    ids = [doc['identifier'] for doc in doc_ids]
+
+    result = termvector_word_cloud(settings.ES_INDEX,
+                                   settings.ES_DOCTYPE,
+                                   ids)
+
+    if not result:
+        return json_response_message('error',
+                                     'No word cloud result generated.')
+
+    return json.dumps(result)
