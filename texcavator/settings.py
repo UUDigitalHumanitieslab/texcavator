6855dc14f4374359da30b93c4bc26e5c1cd7bdfd
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-11-07 14:41:19 +0100

    Add bower.json
    Added a bower.json file to manage javascript dependencies. The following
    dependencies have been installed:
    - angular
    - angular-ui-bootstrap
    - bootstrap-css-only
    Also .gitignore has been updated to include the directory
    bower_components and bower_components was added to Django static
    directories.

--- texcavator/settings.py
+++ texcavator/settings.py
@@ -105,6 +105,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join( PROJECT_ROOT, "static" ) + os.sep,
+    os.path.join(PROJECT_PARENT, "bower_components") + os.sep,
 )
 
 # List of finder classes that know how to find static files in
