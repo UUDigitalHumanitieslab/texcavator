a9d7839a4f3d97883f59f4aff9d127d294110bad
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-11-27 10:30:10 +0100

    Add pagination for search results
    Added pagination for search results.
    
    The pagination does not yet reset when a new query is submitted. This
    should be fixed later.

--- texcavator/static/js/controllers.js
+++ texcavator/static/js/controllers.js
@@ -7,21 +7,37 @@ angular.module('texcavatorApp', ['ui.bootstrap'])
     })
 
     .controller('texcavatorCtrl', ['$scope', '$http', function($scope, $http) {
-        $scope.search = function () {
+        $scope.startRecord = 1;
+        $scope.maxResultsPerPage= 20;
+
+        $scope.search = function (newQuery) {
            console.log('search()');
 
-
            var params = {
-               'query': $scope.queryStr
+               'query': $scope.queryStr,
+               'startRecord': $scope.startRecord,
+               'maximumRecords': $scope.maxResultsPerPage
            };
 
            $http.get('services/search/', {params: params}).
                success(function (data) {
                console.log(data);
                $scope.searchResults = data.data.hits.hits;
+               $scope.totalSearchResults = data.data.hits.total;
+
+
            }).
            error(function (error) {
            });
+        }
+
+        $scope.pageChanged = function () {
+            $scope.startRecord = (($scope.searchResultsPage-1) * $scope.maxResultsPerPage)+1;
+
+            console.log('new page: '+$scope.searchResultsPage);
+            console.log('showing results from: '+$scope.startRecord);
+
+            $scope.search();
         }
     }]);
 
