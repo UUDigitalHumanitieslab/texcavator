d208a7b5de091ed16729596ae9abf99382188404
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-02-23 18:34:12 +0100

    Remove unnecessary settings
    Remove unnecessary settings from settings.py.

--- texcavator/settings.py
+++ texcavator/settings.py
@@ -42,15 +42,6 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -65,11 +56,6 @@ STATIC_ROOT = os.path.join(PROJECT_GRANNY, "texcavator_static") + os.sep
 STATIC_URL = '/static/'
 if DEBUG:
     print >> sys.stderr, "STATIC_URL:", STATIC_URL
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 # add trailing `/'
