8865230c71ea9bffe97f3f1a244a6aa21fb6ee4c
Janneke van der Zwaan <jvdzwaan@texcavator-ui.grid.sara.nl>
2015-03-20 13:59:10 +0100

    Fix encoding issue
    The command failed on accented characters in queries. This error was
    fixed by setting the encoding of stdout.

--- query/management/commands/export_queries.py
+++ query/management/commands/export_queries.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 """Export queries and associated metadata of selected users.
 """
+# fix encoding of output
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
 from django.core.management.base import BaseCommand
 
 from query.models import Query
@@ -25,6 +30,6 @@ class Command(BaseCommand):
                 excl_art_types = [a.id for a in q.exclude_article_types.all()]
                 excl_dist = [d.id for d in q.exclude_distributions.all()]
 
-                print '\t'.join([str(q.id), q.query, str(q.date_lower),
-                                str(q.date_upper), '+'.join(excl_art_types),
-                                '+'.join(excl_dist), str(q.user.id)])
+                print u'{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(q.id, q.query, q.date_lower,
+                                q.date_upper, '+'.join(excl_art_types),
+                                '+'.join(excl_dist), q.user.id)
