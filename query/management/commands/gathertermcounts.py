620c894c7f7ceefb0aa613c5af833fb33b170ce2
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-01-07 16:14:36 +0100

    Rewrite of gathertermcounts management command

--- query/management/commands/gathertermcounts.py
+++ query/management/commands/gathertermcounts.py
@@ -4,9 +4,10 @@ from collections import Counter
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db import transaction
 
 from query.models import Distribution, Term
-from services.es import document_id_chunks, termvector_wordcloud
+from services.es import count_search_results, document_id_chunks, termvector_wordcloud
 from texcavator.utils import daterange2dates
 
 
@@ -18,24 +19,38 @@ class Command(BaseCommand):
         print 'Emptying table...'
         Term.objects.all().delete()
 
+        print 'Retrieving documents...'
         exclude_dist = Distribution.objects.exclude(name='Landelijk').values_list('id', flat=True)
         date_range = daterange2dates('19000101,19901231')
-        sets = document_id_chunks(1000,
+
+        total_documents = count_search_results(settings.ES_INDEX,
+                                               settings.ES_DOCTYPE,
+                                               None,
+                                               date_range,
+                                               exclude_dist, [], []).get('count')
+        print 'Total documents: {}'.format(total_documents)
+
+        sets = document_id_chunks(settings.QUERY_DATA_CHUNK_SIZE,
                                   settings.ES_INDEX,
                                   settings.ES_DOCTYPE,
-                                  'test',
+                                  None,
                                   date_range,
                                   dist=exclude_dist)
 
+        print 'Counting terms...'
         counter = Counter()
-        total_documents = 0
         for n, s in enumerate(sets):
-            counter += termvector_wordcloud(settings.ES_INDEX, settings.ES_DOCTYPE, s, 4)
-            total_documents += len(s)
-            if n == 1:
+            print 'At set {}...'.format(n)
+            counter += termvector_wordcloud(settings.ES_INDEX, settings.ES_DOCTYPE, s, min_length=4)
+            if n == 10:
                 break
 
+        print 'Calculating IDFs...'
+        terms = []
         for term, count in counter.items():
             idf = math.log10(total_documents / float(count))
-            t = Term(word=term, count=count, idf=idf)
-            t.save()
+            terms.append(Term(word=term, count=count, idf=idf))
+        Term.objects.bulk_create(terms)
+
+        print 'Deleting all terms with frequency of 1...'
+        Term.objects.filter(count=1).delete()
