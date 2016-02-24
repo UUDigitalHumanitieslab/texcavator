c901c8ef77257d7d8969f20541deb3125cbd81ae
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-02-24 14:41:36 +0100

    Merge migrations from different branches

---
+++ query/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('query', '0017_auto_20160203_1556'),
+        ('query', '0017_auto_20160224_1306'),
+    ]
+
+    operations = [
+    ]
