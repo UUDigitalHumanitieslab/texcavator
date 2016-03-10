08e3669ab62cd081f7a15a0b2a0eec8127005f99
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-03-10 09:35:05 +0100

    Fixed URL configuration

--- texcavator/urls.py
+++ texcavator/urls.py
@@ -13,48 +13,40 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView, TemplateView
+from django.views.static import serve
 
-import settings
+from .views import index, user_login, user_logout
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^favicon\.ico$',
-        RedirectView.as_view(url='/static/image/icon/favicon.ico')),
+        RedirectView.as_view(url='/static/image/icon/favicon.ico', permanent=False)),
     url(r'^robots\.txt$',
-        TemplateView.as_view(template_name='robots.txt',
-                             content_type='text/plain')),
+        TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 
     # Enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^js/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': 'js/', 'show_indexes': True}),
-    url(r'^$', 'texcavator.views.index', name='index'),
+    # TODO: is this the proper way to refer to locally served JavaScripts?
+    url(r'^js/(?P<path>.*)$', serve, {'document_root': 'js/', 'show_indexes': True}),
 
-    url(r'^login$', 'texcavator.views.user_login'),
-    url(r'^logout$', 'texcavator.views.user_logout'),
+    url(r'^$', index, name='index'),
+    url(r'^login$', user_login),
+    url(r'^logout$', user_logout),
 
     url(r'^services/', include('services.urls')),
     url(r'^query/', include('query.urls')),
-
-    # TODO: Not used? So, remove?
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm', {
-            'template_name': 'registration/password_reset.html',
-            'post_reset_redirect': '/logout/'
-        }),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^500/$', TemplateView.as_view(template_name='500.html')),
-        (r'^404/$', TemplateView.as_view(template_name='404.html')),
-    )
+    urlpatterns.extend([
+        url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500/$', TemplateView.as_view(template_name='500.html')),
+    ])
