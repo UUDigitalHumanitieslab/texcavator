35c5346f9c6fa2de7933e0dcc9575dda453dd508
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-04-07 11:10:32 +0200

    Updated installation script (#108)

--- install-shico.py
+++ install-shico.py
@@ -1,11 +1,25 @@
+import os
+import shutil
 import StringIO
 import zipfile
 
 import requests
 
-r = requests.get('https://github.com/c-martinez/ShiCo/archive/master.zip', stream=True)
+GITHUB_URL = 'https://github.com/c-martinez/ShiCo/archive/demo.zip'
+DIST_DIR = 'ShiCo-demo/webapp/dist/'
+STATIC_DIR = 'texcavator/static/js/'
+TMP_DIR = 'ShiCo-demo'
+FINAL_DIR = 'ShiCo'
+
+# Retrive the demo branch from GitHub
+r = requests.get(GITHUB_URL, stream=True)
 z = zipfile.ZipFile(StringIO.StringIO(r.content))
 
+# Extract files from the dist directory
 for f in z.namelist():
-    if f.startswith('ShiCo-master/webapp/'):
-        z.extract(f, 'texcavator/static/js/')
+    if f.startswith(DIST_DIR):
+        z.extract(f, STATIC_DIR)
+
+# Move the files to the ShiCo folder
+shutil.move(os.path.join(STATIC_DIR, DIST_DIR), os.path.join(STATIC_DIR, FINAL_DIR))
+shutil.rmtree(os.path.join(STATIC_DIR, TMP_DIR))
