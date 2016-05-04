5d4e5df1240161a9715ed6823f60ba4faeb9ee6f
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-05-04 15:08:11 +0200

    Added search rerender on clicking on a day in the heatmap

--- texcavator/static/js/uva/heatmap.js
+++ texcavator/static/js/uva/heatmap.js
@@ -5,12 +5,29 @@ function showHeatmap(query_id, year) {
     cal.init({
         data: 'services/heatmap/' + query_id + '/' + year,
         domain: "month",
-        subDomain: "x_day",
+        subDomain: "day",
         start: new Date(year, 0),
         minDate: new Date(year - 5, 0),
         maxDate: new Date(year + 5, 0),
         range: 12,
         previousSelector: "#cal-heatmap-prev",
         nextSelector: "#cal-heatmap-next",
+        onClick: function(d) {
+            beginDate = d;
+
+            // Sets enddate to the day after the clicked date
+            // Code copied from http://stackoverflow.com/a/3674550/3710392
+            var tomorrow = new Date(d);
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            endDate = tomorrow;
+            
+            dijit.byId("begindate").set("value", beginDate);
+            dijit.byId("enddate").set("value", endDate);
+            if (beginDate2 !== undefined) {
+                toggleSecondDateFilter();
+            }
+            accordionSelectChild("searchPane");
+            searchSubmit();
+        },
     });
 }
