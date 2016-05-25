02ad21687e3f3a5392ac8c39739f973f2a279d59
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-05-25 13:48:22 +0200

    Completed installation script

--- install-shico.py
+++ install-shico.py
@@ -1,26 +1,48 @@
 import os
 import shutil
 import StringIO
+import urlparse
 import zipfile
 
 import requests
 
 VERSION = '0.6'
 GITHUB_URL = 'https://github.com/NLeSC/ShiCo/archive/v{}.zip'.format(VERSION)
-DIST_DIR = 'ShiCo-{}/webapp/dist/'.format(VERSION)
+
 STATIC_DIR = 'texcavator/static/js/'
-TMP_DIR = 'ShiCo-{}'.format(VERSION)
-FINAL_DIR = 'ShiCo'
+DIST = 'ShiCo-{}/webapp/dist/'.format(VERSION)
+DIST_DIR = os.path.join(STATIC_DIR, DIST)
+FINAL_DIR = os.path.join(STATIC_DIR, 'ShiCo')
 
 # Retrieve the archive from GitHub
+print 'Retrieving the archive...'
 r = requests.get(GITHUB_URL, stream=True)
 z = zipfile.ZipFile(StringIO.StringIO(r.content))
 
 # Extract files from the DIST_DIR to the STATIC_DIR
+print 'Unpacking...'
 for f in z.namelist():
-    if f.startswith(DIST_DIR):
+    if f.startswith(DIST):
         z.extract(f, STATIC_DIR)
 
 # Move the files to the ShiCo folder
-shutil.move(os.path.join(STATIC_DIR, DIST_DIR), os.path.join(STATIC_DIR, FINAL_DIR))
-shutil.rmtree(os.path.join(STATIC_DIR, TMP_DIR))
+print 'Moving to ShiCo folder...'
+shutil.rmtree(FINAL_DIR, ignore_errors=True)
+shutil.move(DIST_DIR, STATIC_DIR)
+os.rename(os.path.join(STATIC_DIR, 'dist'), FINAL_DIR)
+shutil.rmtree(os.path.join(STATIC_DIR, 'ShiCo-{}'.format(VERSION)))
+
+# Replace relative paths with absolute paths
+print 'Replacing paths...'
+print os.path.join(FINAL_DIR, 'scripts/app.js')
+with open(os.path.join(FINAL_DIR, 'scripts/app.js'), 'r+') as f:
+    lines = []
+    for line in f:
+        line = line.replace('config.json', urlparse.urljoin(FINAL_DIR, 'config.json'))
+        line = line.replace('/help/algorithm.md', urlparse.urljoin(FINAL_DIR, 'help/algorithm.md'))
+        lines.append(line)
+
+    f.seek(0)
+    f.truncate()
+    for line in lines:
+        f.write(line)
