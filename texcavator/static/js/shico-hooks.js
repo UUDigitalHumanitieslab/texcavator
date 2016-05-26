02f680117075ab68cf99627f61b3fad4f7110d55
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-05-26 08:49:49 +0200

    Setting the date filters on clicking on a term in ShiCo

--- texcavator/static/js/shico-hooks.js
+++ texcavator/static/js/shico-hooks.js
@@ -10,17 +10,17 @@
       // Remove existing text elements
       node.select('text').remove();
 
-      // Create link to function in parent frame
+      // Create link to function to start search for this term and the given year in this frame
       node.append('a')
         .attr('xlink:href', function(d) {
-            return 'javascript:parent.startSearch("' + d.name + '");';
+          return 'javascript:parent.startSearch("' + d.name + '", ' + d.year + ');';
         })
         // Append the text element
         .append('text')
         .attr('dx', '12')
         .attr('dy', '.35em')
         .text(function(d) {
-            return d.name;
+          return d.name;
         });
     });
   }
